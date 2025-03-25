17d3d66f085e9a20f6f534ad2f6be40fade78478
Kovida04 <155882976+Kovida04@users.noreply.github.com>
2025-03-25 14:08:14 +0530

    Time: 525 ms (99.95%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -0,0 +1,8 @@
+Select date_Format (trans_date , "%Y-%m")
+month , country, count(*) trans_count,
+sum(case when state = 'approved' then 1 else 0 end)
+approved_count,
+sum(amount) trans_total_amount,
+sum(case when state = 'approved' then amount else 0 end) approved_total_amount
+from transactions
+group by month,Country;
